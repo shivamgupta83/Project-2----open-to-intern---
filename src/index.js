c4ffb6eda6903cc0a35c505413050413e2e937dc
ritesh12Ri <riteshrajput4487@gmail.com>
2022-11-21 13:23:15 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express  = require("express")
+const bodyParser = require("body-parser")
+const route  = require("./route/route")
+const {default : mongoose} = require("mongoose")
+const app = express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extented:true}))
+
+mongoose.connect("mongodb+srv://ritesh:zbZGz8vHtAKmPfio@newcluster.88v7uq9.mongodb.net/project2",{
+    useNewUrlparser : true
+})
+.then(()=> console.log("mongoDb is connected"))
+.catch((err) => console.log(err))
+
+
+
+app.use("/",route)
+
+app.listen(process.env.PORT ||3000,function(){
+    console.log("server running on port"+" "+ process.env.PORT || 3000 )
+})
+
